b50d4d17ae0581f2457f68247a5ace288793adf6
Alan Plotko <alan.plotko@gmail.com>
2014-09-12 21:49:06 -0400

    Change host to serve on all IPs again

--- app/hello.py
+++ app/hello.py
@@ -1,7 +1,8 @@
+import os
 from bottle import route, run
 
 @route('/')
 def hello():
     return "Hello World!"
 
-run(server='waitress')
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
