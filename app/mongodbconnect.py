8713c5e9478bbc73077b369f9c7e01f05f837466
Alan Plotko <alan.plotko@gmail.com>
2015-08-18 11:20:49 -0400

    Remove development database

--- app/mongodbconnect.py
+++ app/mongodbconnect.py
@@ -3,14 +3,6 @@ import bottle
 import pymongo
 
 def mongoconn():
-	#if in production environment, get URL from environment variable
-	try:
-		MONGO_URL = os.environ.get('MONGOHQ_URL')
-		connection = pymongo.Connection(MONGO_URL)
-		db = connection['cqs_data']
-	# if in development environment, use hardcoded URL
-	except:
-		MONGO_URL = "mongodb://gabeochoa:password1@kahana.mongohq.com:10009/cqs_data"
-		connection = pymongo.Connection(MONGO_URL)
-		db = connection['cqs_data']
-	return db
+	MONGO_URL = os.environ.get('MONGOHQ_URL')
+	connection = pymongo.Connection(MONGO_URL)
+	return connection['cqs_data']
